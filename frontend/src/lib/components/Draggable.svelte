ebcca1dc4aa971220b51e65873db0da1bb525933
Hatz <alextorres4101@gmail.com>
2026-05-16 19:10:02 +0200

    first commit

---
+++ frontend/src/lib/components/Draggable.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    let {left = 0, top = 0, children } = $props();
+	
+	let moving = false;
+	let startX = 0;
+	let startY = 0;
+	let originLeft = 0;
+	let originTop = 0;
+	
+	function onPointerDown(e: PointerEvent) {
+		moving = true;
+
+		startX = e.clientX;
+		startY = e.clientY;
+
+		originLeft = left;
+		originTop = top;
+	}
+	
+	function onPointerMove(e :PointerEvent) {
+		if (moving) {
+			left = originLeft + (e.clientX - startX);
+			top = originTop + (e.clientY - startY);
+		}
+	}
+	
+	function onPointerUp() {
+		moving = false;
+	}
+</script>
+
+<style>
+	.draggable {
+		user-select: none;
+		cursor: move;
+		border: solid 1px gray;
+		position: absolute;
+	}
+</style>
+
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div onpointerdown={onPointerDown} style="left: {left}px; top: {top}px;" class="draggable">
+	{@render children?.()}
+</div>
+
+<svelte:window on:pointerup={onPointerUp} on:pointermove={onPointerMove} />
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+
+	import DraggableDiv from '$lib/components/Draggable.svelte';
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+<DraggableDiv left={100} top={100}>
+	<p>
+		Some content
+	</p>
+</DraggableDiv>
