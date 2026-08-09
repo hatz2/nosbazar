043ba8f5629ca5534f757209fca3b169cfb5f4de
Hatz <alextorres4101@gmail.com>
2026-08-09 17:47:02 +0200

    log: add auth result to logs

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -89,7 +89,7 @@ bool authenticate_and_spawn_clients(Env& env)
             if (auth_result == nosbazar::auth::NosAuth::AuthResult::captcha) {
                 SPDLOG_ERROR("Captcha required - not implemented yet");
             } else {
-                SPDLOG_ERROR("Authentication failed");
+                SPDLOG_ERROR("Authentication failed. Auth result {}", auth_result);
             }
             return false;
         }
